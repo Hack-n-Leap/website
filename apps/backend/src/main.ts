3ece3094974e5368c77d78711a13159943c768ca
ilo80 <ilann.mimet@gmail.com>
2024-03-21 16:42:27 +0100

    Fix point virgule

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get("/", (req, res) => {
-  res.status(404).send("Error, 404 Not Found")
+  res.status(404).send("Error, 404 Not Found");
 });
 
 app.get('/api', (req, res) => {
